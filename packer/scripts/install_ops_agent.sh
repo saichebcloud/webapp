b14d00b2a08bef4f8632594b8db419a2766c716e
Sai Siddhardha Chebrolu <chebrolu.s@northeastern.edu>
2024-03-19 18:08:22 -0400

    modified packer config

---
+++ packer/scripts/install_ops_agent.sh
@@ -0,0 +1,6 @@
+echo "Installing google ops agent"
+
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+
+echo "Completed installing ops agent"
